5b9fa520ca4adcfd54fc9a417a73f77979f5dff1
Nayan <nayanchordia7@gmail.com>
2022-10-15 01:25:18 +0530

    Update test_unit_stydents

--- tests/tests_unit/test_unit_students.py
+++ tests/tests_unit/test_unit_students.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from source.queries import students_queries
 from fastapi import HTTPException
 import pytest
 from main import app
@@ -6,12 +7,30 @@ from main import app
 
 client = TestClient(app)
 
-def test_create_student():
-    res = client.post("/students", json={"age":"6", "first_name": "Sarvesh","family_name": "Patel",
-                                                    "gender": "M",
-                                                    "grade": "A"})
+def id_cache():
+    cache_id = []
+    student = students_queries.select_query()
+    for i in range(0,len(student)):
+        cache_id.append(dict(student[i])['id'])
+    return cache_id
+
+no_id = [10000,10001,-1,-2,0,100000]
+
+
+
+
+@pytest.mark.parametrize("details",[
+    {"age":"6", "first_name": "Sarvesh","family_name": "Patel", "gender": "M", "grade": "A"},
+    {"age":"10", "first_name": "Kanta","family_name": "Soni", "gender": "F", "grade": "B"},
+])
+def test_create_student(details):
+    res = client.post("/students", json=details)
     assert res.status_code == 201
-    assert type(res.json()) == dict
+    res = res.json()
+    del res['id']
+    res['age'] = str(res['age'])
+    assert res == details
+
 
 
 def test_get_students():
@@ -20,36 +39,43 @@ def test_get_students():
     assert type(res.json()) is list
 
 
-def test_get_student_by_id():
-    res = client.get("/students/1")
+@pytest.mark.parametrize("id", [id for id in id_cache()])
+def test_get_student_by_id(id):
+    res = client.get("/students/"+str(id))
     assert res.status_code == 200
-    assert type(res.json()) is dict
+    assert res.json().get('id') == id
 
 
-def test_get_student_by_id_inexistence():
-    res = client.get("/students/500")
+@pytest.mark.parametrize("id", [id for id in no_id])
+def test_get_student_by_id_inexistence(id):
+    res = client.get("/students/"+str(id))
     assert res.status_code == 404
-    assert res.json() == {"detail": "student with 500 was not found"}
+    assert res.json() == {"detail": "student with " + str(id) +  " was not found"}
 
 
-def test_update_student():
-    res = client.patch("/students/1", json={"gender":"F"})
+@pytest.mark.parametrize("id", [121,122,123,124,125,126,127,128,129])
+def test_update_student(id):
+    res = client.patch("/students/"+str(id), json={"gender":"F", "grade":"A"})
     assert res.status_code == 200
-    assert type(res.json()) == dict
+    assert res.json().get('id') == id
+    assert res.json().get('gender') == 'F'
+    assert res.json().get('grade') == 'A'
 
 
-def test_update_student_inexistence():
-    res = client.patch("/students/500", json={"gender":"F"})
+@pytest.mark.parametrize("id", [id for id in no_id])
+def test_update_student_inexistence(id):
+    res = client.patch("/students/"+str(id), json={"gender":"F"})
     assert res.status_code == 404
-    assert res.json() == {"detail": "student with 500 was not found"}
+    assert res.json() == {"detail": "student with " + str(id) +" was not found"}
 
 
-def test_delete_student():
-    res = client.delete("/students/15")
+@pytest.mark.parametrize("id", [113,115,116,117])
+def test_delete_student(id):
+    res = client.delete("/students/"+str(id))
     assert res.status_code == 204
 
-
-def test_delete_student_inexistence():
-    res = client.delete("/students/500")
+@pytest.mark.parametrize("id", [id for id in no_id])
+def test_delete_student_inexistence(id):
+    res = client.delete("/students/"+str(id))
     assert res.status_code == 404
-    assert res.json() == {"detail": "student with 500 was not found"}   
+    assert res.json() == {"detail": "student with " + str(id) +" was not found"}
